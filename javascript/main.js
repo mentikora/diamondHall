7b4c93640aee31093967595b3c7a9dfa5d57f7af
Mentikora <notender_boy@ukr.net>
2016-01-06 13:02:14 +0200

    menu core

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
 
+    var overflow_wr = $('#fixed-menu');
+
+    $('.nav-menu-btn').on('click', function(){
+        overflow_wr.fadeIn();
+    });
+    $('#nav-menu-close-btn').on('click', function(){
+        overflow_wr.fadeOut();
+    });
+
+
+
 });
